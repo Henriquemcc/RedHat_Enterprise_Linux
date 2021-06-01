441ba3fd75f3bec2771b8f65050adc0e6a6fa017
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-06-01 00:02:23 -0300

    Corrigindo erro na função 'configurar_diretorio_home_bin' do arquivo 'InstalarUsuario.py' em que resultaria na escrita de mais de uma vez a string export_bin_string no arquivo '.bashrc'.

--- InstalarUsuario.py
+++ InstalarUsuario.py
@@ -54,9 +54,9 @@ def configurar_diretorio_home_bin():
     # Verificando se o arquivo contem o export path
     with open(path_bash_rc, "r+") as arquivo:
         linhas = arquivo.readlines()
-        if export_bin_string not in linhas:
+        if not any(export_bin_string in linha for linha in linhas):
             arquivo.seek(0, io.SEEK_END)
-            arquivo.write(export_bin_string)
+            arquivo.write("{}\n".format(export_bin_string))
         arquivo.close()
 
 
